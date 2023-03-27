9256ce7707558a340987354402d82c6575040527
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2023-03-27 15:52:02 +0900

    setting: create folder

---
+++ server.js
@@ -0,0 +1,12 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+type Query {
+alltweetes: String
+}
+`
+
+const server = new ApolloServer({ typeDefs })
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`)
+})
